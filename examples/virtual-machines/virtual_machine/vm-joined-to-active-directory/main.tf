f4bd782accff9f17fd4bdca1d21af02d86dce36e
sujan2008 <58722555+sujan2008@users.noreply.github.com>
2023-09-04 15:38:58 +1000

    Update main.tf

--- examples/virtual-machines/virtual_machine/vm-joined-to-active-directory/main.tf
+++ examples/virtual-machines/virtual_machine/vm-joined-to-active-directory/main.tf
@@ -1,9 +1,17 @@
 # Copyright (c) HashiCorp, Inc.
 # SPDX-License-Identifier: MPL-2.0
 
-provider "azurerm" {
-  version = "=3.71.0"
+terraform {
+  required_providers {
+    azurerm = {
+      source  = "hashicorp/azurerm"
+      version = "3.71.0"
+    }
+  }
+  backend "azurerm" {    
+      }
 }
+
 
 locals {
   resource_group_name = "${var.prefix}-resources"
