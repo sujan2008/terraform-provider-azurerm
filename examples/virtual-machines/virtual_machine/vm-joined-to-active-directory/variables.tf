87b2a5c61be996c5e292e01ec805a2f51ee7f8e1
sujan2008 <58722555+sujan2008@users.noreply.github.com>
2023-09-04 14:42:32 +1000

    Update variables.tf

--- examples/virtual-machines/virtual_machine/vm-joined-to-active-directory/variables.tf
+++ examples/virtual-machines/virtual_machine/vm-joined-to-active-directory/variables.tf
@@ -3,12 +3,15 @@
 
 variable "prefix" {
   description = "The prefix used for all resources in this example. Needs to be a short (6 characters) alphanumeric string. Example: `myprefix`."
+  default = "sujan"
 }
 
 variable "admin_username" {
   description = "The username of the administrator account for both the local accounts, and Active Directory accounts. Example: `myexampleadmin`"
+ default = "sujanadmin"
 }
 
 variable "admin_password" {
   description = "The password of the administrator account for both the local accounts, and Active Directory accounts. Needs to comply with the Windows Password Policy. Example: `PassW0rd1234!`"
+  default = "PassW0rd1234!"
 }
